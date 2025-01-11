18562b95a22c21cf9aa8505fc419abd91be6b672
Emigrete <teowelyczko@gmail.com>
2025-01-11 15:12:54 -0300

    Hemos hecho una web de consulta de clima para el curso de JS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const urlBase = `https://api.openweathermap.org/data/2.5/weather`;
-const API_KEY = "302c6129f6687c476d94986497e668da";
+const API_KEY = "API_KEY";
 const diffKelvin = 273.15;
 
 document.getElementById("searchButton").addEventListener("click", () => {
